1736e416a634fd7e18cece0acdfeac652222d8aa
Azeke Work <ultraza3@gmail.com>
2022-06-15 12:20:02 +0600

    some css changes

--- src/Components/ChatUserItem.jsx
+++ src/Components/ChatUserItem.jsx
@@ -5,12 +5,14 @@ import style from "./Chats.module.css"
 const ChatUserItem = (props) => {
     let path = '/MessagesPage/' + props.id;
     return (<>
+        <Link to={path}>
         <div className={props.isActiveUser ? style.ChatItemActive : style.ChatItem}>
             <img className={style.user_avatar} src={props.photoURL} alt="user_avatar" />
             <div className={style.user_name}>
-                <Link to={path}>{props.name}</Link>
+                {props.name}
             </div>
         </div>
+        </Link>
     </>)
 }
 
