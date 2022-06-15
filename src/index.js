62f40bdc343d63ea763bf9833a131bd416e42525
Azeke Home <ultraza3@gmail.com>
2022-06-15 23:44:26 +0600

    trying to deploy on gh-pages 8

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
