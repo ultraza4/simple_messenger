a2be9839924614e07bca66de32c23f52bd213554
Azeke Home <ultraza3@gmail.com>
2022-06-17 22:02:33 +0600

    notifictation system check

--- src/Components/Chats/ChatUserItem.jsx
+++ src/Components/Chats/ChatUserItem.jsx
@@ -8,27 +8,33 @@ const ChatUserItem = (props) => {
     const [newMessages, setNewMessages] = useState([]);
     const [count, setCount] = useState(0);
     const { uid } = auth.currentUser;
-    
+
+    const resetNewMessages = () => {
+        db.collection("textMessages").get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                if (doc.data().sendTo === uid && doc.data().uid === props.id) {
+                    setCount(0);
+                    doc.ref.update({
+                        isNewMessage: false
+                    });
+                }
+            });
+        });
+    }
+
     useEffect(() => {
         db.collection("textMessages")
-            .onSnapshot((snapshot) => {
+            .get().then((snapshot) => {
                 snapshot.docs.forEach((doc) => {
                     if (doc.data().sendTo === uid && doc.data().isNewMessage === true) {
-                        setNewMessages((newMessages) => [...newMessages, doc.data()]);
+                        if (doc.data().uid === props.id) setCount(count => count + 1);
                     }
                 })
             });
-    },[])
-    
-    useEffect(() => {
-        newMessages.forEach((message) => {
-            if (message.uid === props.id) setCount(count => count + 1);
-        })
-        debugger;
-    }, [newMessages]);
+    }, [])
 
     return (<>
-        <Link to={path}>
+        <Link onClick={resetNewMessages} to={path}>
             <div className={props.isActiveUser ? style.ChatItemActive : style.ChatItem}>
                 <img className={style.user_avatar} src={props.photoURL} alt="user_avatar" />
                 <div className={style.user_name}>
